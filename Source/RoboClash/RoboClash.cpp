5bcce09787c9e85e659bd0c480120c9c834638c9
Nikola Ilic <n1kol4n1k@gmail.com>
2024-07-23 22:38:47 +0200

    First commit
    Initialized project

---
+++ Source/RoboClash/RoboClash.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RoboClash.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RoboClash, "RoboClash" );
