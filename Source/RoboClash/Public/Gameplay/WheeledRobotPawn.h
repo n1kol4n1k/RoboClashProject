2b14dd227664786b8b0a041b43a54bef7887364d
Nikola Ilic <n1kol4n1k@gmail.com>
2024-07-24 23:05:21 +0200

    Added new classes

---
+++ Source/RoboClash/Public/Gameplay/WheeledRobotPawn.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehiclePawn.h"
+#include "WheeledRobotPawn.generated.h"
+
+UCLASS()
+class ROBOCLASH_API AWheeledRobotPawn : public AWheeledVehiclePawn
+{
+	GENERATED_BODY()
+	
+};
