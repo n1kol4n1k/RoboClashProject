d69a1ff6403e11c0aff76ce72723263234f89e39
Nikola Ilic <n1kol4n1k@gmail.com>
2024-08-01 20:07:30 +0200

    Added weapon & health logic

--- Source/RoboClash/Private/Gameplay/WheeledRobotPawn.cpp
+++ Source/RoboClash/Private/Gameplay/WheeledRobotPawn.cpp
@@ -4,3 +4,40 @@ float AWheeledRobotPawn::CalculateCurrentWeaponAngle(float DeltaTime)
 {
 	return 0.f;
 }
+
+void AWheeledRobotPawn::HandleStartWeaponInput()
+{
+	mWeaponState = RobotWeaponState::Active;
+}
+
+void AWheeledRobotPawn::HandleEndWeaponInput()
+{
+	mWeaponState = RobotWeaponState::Inactive;
+}
+
+void AWheeledRobotPawn::NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (mWeaponState == RobotWeaponState::Inactive)
+	{
+		return;
+	}
+
+	if (Hit.BoneName != mWeaponBoneName)
+	{
+		return;
+	}
+
+	if (const AWheeledRobotPawn* OtherRobot = Cast<AWheeledRobotPawn>(Other))
+	{
+		mCurrentHealth -= OtherRobot->GetWeaponHitDamage();
+		UpdateHealthUI(mCurrentHealth, mCurrentHealth / mStartingHealth);
+	}
+}
+
+void AWheeledRobotPawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+	mCurrentHealth = mStartingHealth;
+	UpdateHealthUI(mCurrentHealth, mCurrentHealth/mStartingHealth);
+}
--- Source/RoboClash/Public/Gameplay/WheeledRobotPawn.h
+++ Source/RoboClash/Public/Gameplay/WheeledRobotPawn.h
@@ -22,7 +22,33 @@ protected:
 	RobotWeaponState mWeaponState = RobotWeaponState::Active;
 	float mCurrentWeaponAngle = 0.f;
 
+	UPROPERTY(EditAnywhere)
+	FName mWeaponBoneName;
+
+	UPROPERTY(EditAnywhere)
+	float mStartingHealth;
+	
+	UPROPERTY(EditAnywhere) //move to transient after implementing ui
+	float mCurrentHealth;
+
+	UPROPERTY(EditAnywhere)
+	float mWeaponHitDamage;
+
 public:
+	float GetWeaponHitDamage() const { return mWeaponHitDamage; }
+
 	UFUNCTION(BlueprintCallable)
 	virtual float CalculateCurrentWeaponAngle(float DeltaTime);
+
+	UFUNCTION(BlueprintCallable)
+	void HandleStartWeaponInput();
+
+	UFUNCTION(BlueprintCallable)
+	void HandleEndWeaponInput();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void UpdateHealthUI(float CurrHealth, float CurrHealthPercentage);
+
+	virtual void NotifyHit(class UPrimitiveComponent* MyComp, AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) override;
+	virtual void BeginPlay() override;
 };
